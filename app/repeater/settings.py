eaa18d1467402dd939e45d77bf252b0ef16cc628
tarxvf <git@tarxvf.tech>
2022-10-12 23:32:53 -0400

    ah. try again

--- app/repeater/settings.py
+++ app/repeater/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ DEV_SECRET_KEY = 'django-insecure-=2_vwnsj0#_bn40*w+ug*^-(=e#50pnjv9v&i2cuxqx%5u
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', DEV_SECRET_KEY)
 STATIC_URL = '/s/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/_staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
