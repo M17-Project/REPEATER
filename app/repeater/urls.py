165fd1c58dd464a79941be35f4b882466e48b39f
tarxvf <git@tarxvf.tech>
2022-10-13 01:21:51 -0400

    kludge around fly being slow to serve static files on deployment

--- app/repeater/urls.py
+++ app/repeater/urls.py
@@ -13,13 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('rdb/', include('rdb.urls')),
     path('', include('rdb.urls')),
+    re_path(r"^" +settings.STATIC_URL.strip('/')  + r"/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}), 
+    #fly static files can take a long time to
+    # settle on a deploy, and meanwhile django will 404 otherwise. So
+    # we don't expect to serve static files all the time in production
+    # with this rule, but it is necessary at least some times because of
+    # fly.io idiosyncrasy.
 ]
 urlpatterns += staticfiles_urlpatterns()
